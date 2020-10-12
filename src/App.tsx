chulman444/alimotih-first-solution-prep

commit bb84ab878897b4665916f5b6a780207603bda49c

Pause 'live'
Pauses the timer while the popup is open when there is no valid img to
click.

src/App.tsx
@@ -36,6 +36,14 @@ class App extends React.Component<any, any> {
         })
       })
     })
+    
+    chrome.runtime.getBackgroundPage((bgWindow) => {
+      if(bgWindow) {
+        bgWindow.addEventListener("pause", () => {
+          this.pauseTimer()
+        })
+      }
+    })
   }
   
   updateImg() {

src/background.ts
@@ -51,6 +51,7 @@ chrome.runtime.onMessage.addListener((message, sender, cb) => {
       results[tab_id].state = "paused"
       
       chrome.storage.local.set({ [String(tab_id)]: results[tab_id] }, () => {
+        window.dispatchEvent(new Event("pause"))
         /**
          * 2020-10-08 10:09
          * 

src/contentScript.ts
@@ -73,9 +73,7 @@ function startAutoClick(tab_id:number, wait_milisec:number) {
       
       chrome.runtime.sendMessage({ action: "getMinImgArea", tab_id }, ({ min_img_area: old_min_img_area }) => {
         if(min_img_area < old_min_img_area * 1/3) {
-          chrome.runtime.sendMessage({ action: "pause", tab_id }, () => {
-            console.log(`Img too small. Paused`)
-          })
+          chrome.runtime.sendMessage({ action: "pause", tab_id }, () => {})
         }
         else {
           chrome.runtime.sendMessage({ action: "setMinImgArea", min_img_area, tab_id }, () => {

commit 6c200ba8018700c8076a9babab3c141e09c6ec0c

Fix bug introduced in `bb84ab8`
The timer won't go off the second time. This bug was introduced in
bb84ab8 due to infinite loop of the `pause` event between the popup
script and the background script.

src/App.tsx
@@ -42,7 +42,16 @@ class App extends React.Component<any, any> {
     chrome.runtime.getBackgroundPage((bgWindow) => {
       if(bgWindow) {
         bgWindow.addEventListener("pause", () => {
-          this.pauseTimer()
+          /**
+           * 2020-10-12 11:07
+           * A bug was introduced in `bb84ab8`. It was a infinite loop of this event being fired.
+           * The main reason is the the `pauseTimer` is called even when the state is already paused.
+           * 
+           * Consider checking for the `this.state.state` in each `pauseTimer` and `startTimer` later.
+           */
+          if(this.state.state == "start") {
+            this.pauseTimer()
+          }
         })
       }
     })

src/background.ts
@@ -31,6 +31,15 @@ chrome.runtime.onMessage.addListener((message, sender, cb) => {
     const timer_id = message.timer_id
     
     chrome.storage.local.get([String(tab_id)], (results) => {
+      /**
+       * 2020-10-12 11:11
+       * 
+       * Double prevention side effect of bug introduced in `bb84ab8`
+       */
+      if(results[tab_id].state == "start") {
+        return
+      }
+      
       results[tab_id].timer_ids.push(timer_id)
       results[tab_id].state = "start"
       results[tab_id].start_dt = message.start_dt
@@ -44,6 +53,15 @@ chrome.runtime.onMessage.addListener((message, sender, cb) => {
   }
   else if(action == "pause") {
     chrome.storage.local.get([String(tab_id)], (results) => {
+      /**
+       * 2020-10-12 11:11
+       * 
+       * Double prevention of bug introduced in `bb84ab8`
+       */
+      if(results[tab_id].state == "paused") {
+        return
+      }
+      
       const timer_ids = (results[tab_id].timer_ids as Array<any>).slice()
       
       results[tab_id].timer_ids = []